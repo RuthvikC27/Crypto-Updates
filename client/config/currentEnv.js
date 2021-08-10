6b348fab3aa22513246e43c822242b84a48931ab
comet <comet@pop-os.localdomain>
2021-08-10 23:48:11 +0530

    initial

--- client/config/currentEnv.js
+++ client/config/currentEnv.js
@@ -1,4 +1,4 @@
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production';
 // console.log(dev);
 
 export const currentEnv = dev ? 'http://localhost:3000' : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
