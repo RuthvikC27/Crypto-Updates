81a533aa6be562d53120fcba85fad30378922546
comet <comet@pop-os.localdomain>
2021-08-10 22:48:14 +0530

    initial

--- client/config/currentEnv.js
+++ client/config/currentEnv.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 // console.log(dev);
 
-export const currentEnv = dev ? 'http://localhost:3000' : 'cryptoupdates-ten.vercel.app';
+export const currentEnv = dev ? 'http://localhost:3000' : 'https://cryptoupdates-ten.vercel.app';
